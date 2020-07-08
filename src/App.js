b72a1a6b21479ec12e91bcf2040902a7bd5bae07
joseluizma <joseluizma@hotmail.com>
2020-07-07 22:45:38 -0300

    Finalizado Tarefa

--- src/App.js
+++ src/App.js
@@ -22,55 +22,63 @@ export default function App() {
     });
   }, []);
 
-
   async function handleLikeRepository(id) {
-    await api.post('/repositories/' + id / 'like');
+
+    const response = await api.post('repositories/' + id + '/like');
+
+    const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+    if (repositorieIndex < 0) {
+      return response.status(400).json({ error: 'Repositorie not found.' });
+    }
+
+    const repositorie = repositories[repositorieIndex];
+    repositorie.likes = repositories[repositorieIndex].likes + 1;
+    repositories[repositorieIndex] = repositorie;
+
+    setRepositories([...repositories]);
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
+        <FlatList
+          data={repositories}
+          keyExtractor={repositorie => repositorie.id}
+          renderItem={({ item: repositorie }) => (
 
-          {repositories.map(repositorie => (
-            <Text style={styles.repository} key={repositorie.id}>{repositorie.title}</Text>
-          ))}
+            <View style={styles.repositoryContainer}>
 
-          <View style={styles.techsContainer}>
-            {/* <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text> */}
+              <Text style={styles.repository} >{repositorie.title}</Text>
 
-            {repositories.map(repositorie => (
-            <Text style={styles.tech} key={repositorie.id}>{repositorie.techs}</Text>
-          ))}
-          </View>
+              <View style={styles.techsContainer}>
+                <FlatList
+                  data={repositorie.techs}
+                  renderItem={({ item: repositorieTechs }) => (
+                    <Text style={styles.tech} >{repositorieTechs}</Text>
+                  )}
+                />
+              </View>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+              <View style={styles.likesContainer}>
+                <Text style={styles.likeText} testID={`repository-likes-${repositorie.id}`}>{repositorie.likes} curtidas</Text>
+              </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repositorie.id)}
+                testID={`like-button-${repositorie.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+
+            </View>
+          )}
+        />
       </SafeAreaView>
     </>
+
   );
 }
 
